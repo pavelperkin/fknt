d1f967137ad0b029f943623b782cbb294f36150c
pavelperkin <perkinpv@mail.ru>
2013-06-06 19:53:01 +0300

    view changes 2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,5 @@
 class GroupsController < ApplicationController
-  # GET /groups
-  # GET /groups.json
+
   def index
     @groups_1 = Group.where(:course => 1)
     @groups_2 = Group.where(:course => 2)
@@ -8,80 +7,37 @@ class GroupsController < ApplicationController
     @groups_4 = Group.where(:course => 4)
     @groups_5 = Group.where(:course => 5)
     @groups_6 = Group.where(:course => 6)
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @groups }
-    end
   end
 
-  # GET /groups/1
-  # GET /groups/1.json
   def show
     @group = Group.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @group }
-    end
   end
 
-  # GET /groups/new
-  # GET /groups/new.json
   def new
     @group = Group.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @group }
-    end
   end
 
-  # GET /groups/1/edit
   def edit
     @group = Group.find(params[:id])
   end
 
-  # POST /groups
-  # POST /groups.json
   def create
     @group = Group.new(params[:group])
-
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to @group, notice: 'Group was successfully created.' }
-        format.json { render json: @group, status: :created, location: @group }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
+    @group.save
+    @group.schedules.create({:schedule_type_id => 1})
+    @group.schedules.create({:schedule_type_id => 2})
+    redirect_to @group, notice: 'Group was successfully created.'
   end
 
-  # PUT /groups/1
-  # PUT /groups/1.json
   def update
     @group = Group.find(params[:id])
-
-    respond_to do |format|
-      if @group.update_attributes(params[:group])
-        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
+    @group.update_attributes(params[:group])
+    redirect_to @group, notice: 'Group was successfully updated.'
   end
 
-  # DELETE /groups/1
-  # DELETE /groups/1.json
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
-
-    respond_to do |format|
-      format.html { redirect_to groups_url }
-      format.json { head :no_content }
-    end
+    redirect_to groups_url
   end
 end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,80 +2,23 @@ class SchedulesController < ApplicationController
   
   respond_to :html, :js
 
-  def index
-    @schedules = Schedule.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @schedules }
-    end
-  end
-
-
   def show
-    #@schedule = Schedule.find(params[:id])
-    # @schedule = Schedule.where(:group_id => current_user.group.id)
-    @schedules = Schedule.where(:group_id => 1)
-
+    @schedule = Schedule.find(params[:id])
   end
 
-  # GET /schedules/new
-  # GET /schedules/new.json
-  def new
-    @schedule = Schedule.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @schedule }
-    end
-  end
-
-  # GET /schedules/1/edit
   def edit
     @schedule = Schedule.find(params[:id])
   end
 
-  # POST /schedules
-  # POST /schedules.json
-  def create
-    @schedule = Schedule.new(params[:schedule])
-
-    respond_to do |format|
-      if @schedule.save
-        format.html { redirect_to @schedule, notice: 'Schedule was successfully created.' }
-        format.json { render json: @schedule, status: :created, location: @schedule }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @schedule.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /schedules/1
-  # PUT /schedules/1.json
   def update
     @schedule = Schedule.find(params[:id])
-
-    respond_to do |format|
-      if @schedule.update_attributes(params[:schedule])
-        format.html { redirect_to @schedule, notice: 'Schedule was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @schedule.errors, status: :unprocessable_entity }
-      end
-    end
+    @schedule.update_attributes(params[:schedule])
+    redirect_to @schedule, notice: 'Schedule was successfully updated.'
   end
 
-  # DELETE /schedules/1
-  # DELETE /schedules/1.json
   def destroy
     @schedule = Schedule.find(params[:id])
     @schedule.destroy
-
-    respond_to do |format|
-      format.html { redirect_to schedules_url }
-      format.json { head :no_content }
-    end
+    redirect_to schedules_url
   end
 end
