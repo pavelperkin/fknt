056d0f4646ecc562f4b55a64cee9757343d659f4
pavelperkin <perkinpv@mail.ru>
2013-06-09 13:14:12 +0300

    deploy

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ActiveRecord::Base
   has_many :schedules
   attr_accessible :course, :name
 
-  validates :course, :numericality => { :greater_than => 0, :less_than_or_equal_to => 6 }
+  validates :course, :numericality => { :greater_than__or_equal_to => 0, :less_than_or_equal_to => 6 }
   
 end
